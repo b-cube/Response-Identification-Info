ed6653af64815f81ca8eab2dc42fef59706d0139
roomthily <sorenscott@gmail.com>
2015-10-23 09:39:05 -0600

    ipy for bag of words (very basic) counts

---
+++ scripts/sqls/flatten_completeness.sql
@@ -0,0 +1,19 @@
+﻿with i as 
+(
+	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+	from identities d
+	where d.identity is not null
+)
+select r.source_url, r.metadata_age, i.ident->'protocol' as protocol, 
+	m.completeness->'lineage' as has_lineage,
+	m.completeness->'data_quality' as has_quality,
+	m.completeness->'metadata_ref' as has_metadata,
+	m.completeness->'data_quality_bow' as dataquality_wordcount,
+	m.completeness->'lineage_bow' as lineage_wordcount,
+	m.completeness->'distribution'->'online_refs' as online_distributions,
+	m.completeness->'distribution'->'offline_refs' as offline_distributions,
+	m.completeness->'distribution'->'nondigital_refs' as nondigital_distributions
+from responses r 
+	join metadata_age_metrics as m on m.response_id = r.id
+	join i on i.response_id = r.id
+limit 100;
