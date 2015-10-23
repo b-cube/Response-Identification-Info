f3f770cc5fb64380f46b0fc0a11afa3692a410c9
roomthily <sorenscott@gmail.com>
2015-10-23 16:41:28 -0600

    collecting analysis in ipy, exclude rdf from bow

--- scripts/sqls/flatten_completeness.sql
+++ scripts/sqls/flatten_completeness.sql
@@ -14,6 +14,5 @@ select r.source_url, r.metadata_age, i.ident->'protocol' as protocol,
 	m.completeness->'distribution'->'offline_refs' as offline_distributions,
 	m.completeness->'distribution'->'nondigital_refs' as nondigital_distributions
 from responses r 
-	join metadata_age_metrics as m on m.response_id = r.id
-	join i on i.response_id = r.id
-limit 100;
+	right outer join metadata_age_metrics as m on m.response_id = r.id
+	join i on i.response_id = r.id;
