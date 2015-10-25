bfd32387c488a97c8cee422b144d5792a2851b02
roomthily <sorenscott@gmail.com>
2015-10-25 17:48:14 -0600

    script to post online ref outputs

---
+++ scripts/update_metrics.py
@@ -0,0 +1,35 @@
+import glob
+import sqlalchemy as sqla
+from sqlalchemy.orm import sessionmaker
+import json as js
+from mpp.models import Metric
+
+with open('big_rds.conf', 'r') as f:
+    conf = json.loads(f.read())
+
+# our connection
+engine = sqla.create_engine(conf.get('connection'))
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+# it's under scripts (forgot, but ephemera)
+files = glob.glob('outputs/online_refs/*.json')
+
+for f in files[:5]:
+    response_id = int(f.split('/')[-1].replace('.json', ''))
+    print response_id
+    with open(f, 'r') as g:
+        data = js.loads(g.read())
+
+    metric = session.query(Metric).filter(Metric.response_id==response_id).first()
+
+    if not metric:
+        print 'failed query', response_id
+        continue
+
+    metric.completeness.update({"extracted_online_refs": data})
+    try:
+        session.commit()
+    except:
+        session.rollback()
