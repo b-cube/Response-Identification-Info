c8323bc466aa0a81f651d60da1fc2f90ae8e5973
roomthily <sorenscott@gmail.com>
2015-10-27 00:19:02 -0600

    and the wrapper to handle the bags of words

---
+++ scripts/run_wordcounts.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+import json as js  # name conflict with sqla
+import sqlalchemy as sqla
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import and_, or_, not_
+from mpp.models import Response
+from mpp.models import BagOfWords
+from semproc.timed_command import TimedCmd
+
+'''
+wrapper for the singleton bag of words cli
+mostly to kill any wonky regex
+
+note: if the process is terminated, the bag
+is empty - no partial resultset
+'''
+
+
+conf = '../local/big_rds.conf'
+cmd = 'python word_count_cli.py -c %(config)s -i %(response_id)s'
+timeout = 120  # in seconds, more than 2minutes seems like an eternity
+
+with open(conf, 'r') as f:
+    config = js.loads(f.read())
+
+# our connection
+engine = sqla.create_engine(conf.get('connection'))
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
+
+clauses = [
+    Response.format == 'xml',
+    not_(or_(
+        Response.cleaned_content.startswith("<rdf"),
+        Response.cleaned_content.startswith("<RDF")
+    ))
+]
+
+# get a count of the xml responses
+TOTAL = session.query(Response).filter(
+    and_(*clauses)).count()
+START = 0
+LIMIT = 100
+
+for i in xrange(START, TOTAL, LIMIT):
+    response_ids = session.query(Response.id).filter(
+        and_(*clauses)).limit(LIMIT).offset(i).all()
+
+    for response_id in response_ids:
+        tc = TimedCmd(cmd % {"config": conf, "response_id": response_id})
+        try:
+            status, output, error = tc.run(timeout)
+        except:
+            print 'failed extraction: ', response_id
+            continue
+
+        bag = BagOfWords(
+            generated_on=datetime.now(),
+            bag_of_words=output.split(),
+            method="basic",
+            response_id=response_id
+        )
+
+        try:
+            session.add(bag)
+            session.commit()
+        except Exception as ex:
+            print 'failed commit: ', response_id, ex
+            session.rollback()
+
+session.close()
