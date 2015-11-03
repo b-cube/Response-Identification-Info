c1b560c5621559770c108c7e8908ee784ec274b9
roomthily <sorenscott@gmail.com>
2015-11-02 22:07:50 -0700

    working. not a good urn v url option
    the url regex does not require the forward slashes.

---
+++ scripts/sqls/delete_duplicate_validations.sql
@@ -0,0 +1,6 @@
+﻿DELETE FROM validations
+WHERE id IN (SELECT id
+FROM (SELECT id,
+	     ROW_NUMBER() OVER (partition BY response_id ORDER BY id) AS rnum
+     FROM validations) t
+WHERE t.rnum > 1);
