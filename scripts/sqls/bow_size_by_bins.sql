f4dfa3505f4d69f56ec937d696b8bb6af34348af
roomthily <sorenscott@gmail.com>
2015-11-04 10:15:47 -0700

    bag of words stats in the ipy

---
+++ scripts/sqls/bow_size_by_bins.sql
@@ -0,0 +1,58 @@
+﻿-- -- PULL ALL records with an identity
+-- with i
+-- as (
+-- 	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+-- 	from identities d
+-- 	where d.identity is not null
+-- )
+-- 
+-- select r.source_url, i.ident->'protocol' as protocol, r.metadata_age, array_length(b.bag_of_words, 1) as bow_size
+-- from responses r join i on i.response_id = r.id
+-- 	join bags_of_words b on b.response_id = r.id;
+
+
+-- -- Let's look at the ISO/FGDC buckets over time
+-- with i
+-- as (
+-- 	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+-- 	from identities d
+-- 	where d.identity is not null
+-- )
+-- 
+-- select extract(year from r.metadata_age) as year, avg(array_length(b.bag_of_words, 1))::int as annual_avg_bow_size
+-- from responses r join i on i.response_id = r.id
+-- 	join bags_of_words b on b.response_id = r.id
+-- where i.ident->>'protocol' = 'FGDC' and r.metadata_age is not null
+-- group by year
+-- order by year;
+
+-- -- Let's look at the ISO/FGDC buckets over time, grouped by protocol
+with i
+as (
+	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+	from identities d
+	where d.identity is not null
+)
+
+select i.ident->'protocol' as protocol,
+	extract(year from r.metadata_age) as year,
+	avg(array_length(b.bag_of_words, 1))::int as annual_avg_bow_size
+from responses r join i on i.response_id = r.id
+	join bags_of_words b on b.response_id = r.id
+where r.metadata_age is not null
+group by protocol, year
+order by protocol, year;
+
+-- -- we can't bin by year for ogc and opensearch
+-- -- so just average the things
+-- with i
+-- as (
+-- 	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+-- 	from identities d
+-- 	where d.identity is not null
+-- )
+-- 
+-- select i.ident->'protocol' as protocol, avg(array_length(b.bag_of_words, 1))::int as annual_avg_bow_size
+-- from responses r join i on i.response_id = r.id
+-- 	join bags_of_words b on b.response_id = r.id
+-- group by protocol;
