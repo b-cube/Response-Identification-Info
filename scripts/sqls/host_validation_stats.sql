61e9dc5ce5955b54009c9fe5764b2e70a8ff3e4a
roomthily <sorenscott@gmail.com>
2015-11-04 01:23:39 -0700

    validations by schemas and hosts

---
+++ scripts/sqls/host_validation_stats.sql
@@ -0,0 +1,13 @@
+﻿with s as (
+	select id, metadata_age, host
+	from responses
+	where host is not null and host != ''
+)
+
+select s.host, count(s.host) as number_of_hosts, avg(array_length(v.errors,1))::int as average_errors,
+	max(array_length(v.errors,1))::int as max_errors,
+	min(array_length(v.errors,1))::int as min_errors
+from s join validations v on v.response_id = s.id
+where v.valid = False
+group by s.host
+order by average_errors desc;
---
+++ scripts/sqls/schema_validation_stats.sql
@@ -0,0 +1,13 @@
+﻿with s as (
+	select id, metadata_age, schemas
+	from responses
+	where schemas is not null and schemas != '{}'
+)
+
+select array_to_string(s.schemas, ' | ') as schema_set, count(s.schemas) as number_of_schemas, avg(array_length(v.errors,1))::int as average_errors,
+	max(array_length(v.errors,1))::int as max_errors,
+	min(array_length(v.errors,1))::int as min_errors
+from s join validations v on v.response_id = s.id
+where v.valid = False
+group by s.schemas
+order by average_errors desc;
