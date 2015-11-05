07db9557e3cc0739721369cc561bebde671e6550
roomthily <sorenscott@gmail.com>
2015-11-04 17:43:09 -0700

    some additional bits about responses per host
    and init open search ipy.

--- scripts/sqls/osdd_selects.sql
+++ scripts/sqls/osdd_selects.sql
@@ -7,11 +7,20 @@
 -- where namespaces::jsonb ?| array['time', 'geo'];
 -- 	--and namespaces->>'default' != 'http://www.w3.org/XML/1998/namespace';
 
-with ns as(
-   select url, json_each_text(namespaces) as kvps
-   from osdds
-)
+-- with ns as(
+--    select url, json_each_text(namespaces) as kvps
+--    from osdds
+-- )
+-- 
+-- select kvps, count(kvps)
+-- from ns
+-- group by kvps;
 
-select kvps, count(kvps)
-from ns
-group by kvps;
+
+
+select count(distinct o.id)
+from osdds o
+where namespaces::jsonb->>
+
+
+--kvps.value = 'http://a9.com/-/opensearch/extensions/geo/1.0/' or kvps.value = 'http://a9.com/-/opensearch/extensions/time/1.0/';
