b9b5fb5a0928b9c244ec102722361eb626c64c44
roomthily <sorenscott@gmail.com>
2015-11-06 11:07:13 -0700

    things that don't respect head requests
    trying to be friendly here and you’re not making it easy.

--- scripts/sqls/service_linkrot.sql
+++ scripts/sqls/service_linkrot.sql
@@ -19,6 +19,37 @@
 -- group by protocol, code, harvest_month
 -- order by protocol, code, date_trunc('month', r.initial_harvest_date)::date ASC;
 
+-- -- linkrot by protocol and harvest month
+-- with i
+-- as (
+--     select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+--     from identities d
+--     where d.identity is not null
+-- )
+-- 
+-- select i.ident->'protocol' as protocol, count(i.ident->'protocol'),
+-- 	date_trunc('month', r.initial_harvest_date)::date as harvest_month
+-- from responses r join i on r.id = i.response_id
+-- where i.ident->>'protocol' = 'OGC' or i.ident->>'protocol' = 'OpenSearch'
+-- group by protocol, harvest_month
+-- order by protocol, date_trunc('month', r.initial_harvest_date)::date ASC;
+
+-- -- links by harvest month
+-- with i
+-- as (
+--     select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+--     from identities d
+--     where d.identity is not null
+-- )
+-- 
+-- select i.ident->'protocol' as protocol,
+-- 	date_trunc('month', r.initial_harvest_date)::date as harvest_month, 
+-- 	r.source_url,
+-- 	round(s.status, -2) as status_code
+-- from responses r join i on r.id = i.response_id
+-- 	join service_linkrot s on s.response_id = r.id
+-- where i.ident->>'protocol' = 'OGC' or i.ident->>'protocol' = 'OpenSearch'
+-- order by protocol, date_trunc('month', r.initial_harvest_date)::date ASC;
 
 with i
 as (
@@ -27,9 +58,10 @@ as (
     where d.identity is not null
 )
 
-select i.ident->'protocol' as protocol, count(i.ident->'protocol'),
-	date_trunc('month', r.initial_harvest_date)::date as harvest_month
+select date_trunc('month', r.initial_harvest_date)::date as harvest_month, 
+	r.source_url,
+	round(s.status, -2) as status_code
 from responses r join i on r.id = i.response_id
-where i.ident->>'protocol' = 'OGC' or i.ident->>'protocol' = 'OpenSearch'
-group by protocol, harvest_month
-order by protocol, date_trunc('month', r.initial_harvest_date)::date ASC;
+	join service_linkrot s on s.response_id = r.id
+where i.ident->>'protocol' = 'OGC' and round(s.status, -2) > 200 and round(s.status, -2) < 900 
+order date_trunc('month', r.initial_harvest_date)::date ASC;
