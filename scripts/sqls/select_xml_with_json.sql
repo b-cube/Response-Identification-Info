885d3a177763a0ce798b03c9409cbd813d965154
roomthily <sorenscott@gmail.com>
2015-11-07 22:33:45 -0700

    unique ids, json in the xml ipy

--- scripts/sqls/select_xml_with_json.sql
+++ scripts/sqls/select_xml_with_json.sql
@@ -1,12 +1,39 @@
-﻿with i as 
+﻿-- with i as 
+-- (
+-- 	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+-- 	from identities d
+-- 	where d.identity is not null
+-- )
+
+-- select r.host, count(r.source_url) as responses_with_json --i.ident->'protocol' as protocol, 
+-- --x.extracted_json, x.xpath
+-- from responses r 
+-- 	join xml_with_jsons x on x.file = r.source_url_sha
+-- 	--join i on i.response_id = r.id
+-- --order by i.ident->>'protocol';
+-- 
+-- group by r.host
+-- order by responses_with_json DESC;
+
+
+-- select x.xpath, count(x.xpath) as xpaths 
+-- from responses r 
+--     join xml_with_jsons x on x.file = r.source_url_sha
+-- group by x.xpath
+-- order by xpaths DESC;
+
+with i as 
 (
 	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
 	from identities d
 	where d.identity is not null
 )
 
-select r.source_url, i.ident->'protocol' as protocol, x.extracted_json, x.xpath
+select x.xpath,x.extracted_json, i.ident->'protocol' as protocol
+--x.extracted_json, x.xpath
 from responses r 
 	join xml_with_jsons x on x.file = r.source_url_sha
 	join i on i.response_id = r.id
-order by i.ident->>'protocol';
+--order by i.ident->>'protocol';
+
+order by x.xpath;
--- scripts/unique_identifier_cli.py
+++ scripts/unique_identifier_cli.py
@@ -1,12 +1,12 @@
+#!/anaconda/bin/python
 # -*- coding: utf-8 -*-
 
 from optparse import OptionParser
-import json as js
-import traceback
-
+import json
 import sys
-
+import os
 from semproc.unique_identifiers import IdentifierExtractor
+from semproc.nlp_utils import load_token_list
 
 import warnings
 warnings.filterwarnings('ignore')
@@ -35,10 +35,45 @@ def main():
     with open(options.file, 'r') as f:
         xml_as_string = f.read()
 
-    extractor = IdentifierExtractor(options.url, xml_as_string)
+    # TODO: don't hardcode this
+    # set up the exclude lists
+    equality = []
+    for f in ['namespaces.txt']:
+        equality += load_token_list(f)
 
+    contains = []
+    for f in ['cat_interop_urns.txt', 'mimetypes.txt', 'namespaces.txt', 'excludes_by_contains.txt']:
+        contains += load_token_list(f)
+
+    tag_paths = [
+        '@codeList',
+        '@schemaLocation',
+        'identifier/@type',
+        'Details/Licence',
+        'binding/@transport',
+        '@template',
+        '@uri-type',
+        'rights/@resource',
+        'digform/digtinfo/formspec',
+        'license/@href',
+        'definition/@href',
+        'dateTime/formatString',
+        'RDF/Description/@about',
+        'Binary/Thumbnail/Data'  # to exclude base64 encoded data
+    ]
+
+    extractor = IdentifierExtractor(
+        options.url,
+        xml_as_string,
+        equality,
+        contains,
+        tag_paths
+    )
+
+    identifiers = []
     try:
-        extractor.process_text()
+        for ident in extractor.process_text():
+            identifiers.append(ident)
     except Exception as ex:
         if debug:
             sys.stderr.write('Failed extraction: {0}'.format(ex))
@@ -46,7 +81,10 @@ def main():
             sys.stderr.write('Failed extraction')
         sys.exit(1)
 
-    # repack the things
+    # repack the things to send as strings back up the cli chain
+    # i think as a stringified json dict, streaming json style -
+    # one dict per line, one dict per identifier
+    print '\n'.join([json.dumps(ident.to_json()) for ident in identifiers])
 
 
 if __name__ == '__main__':
