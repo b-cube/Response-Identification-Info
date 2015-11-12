04e10bf4dfbd19b335c85c434b3b8f249ac80fd7
roomthily <sorenscott@gmail.com>
2015-11-12 00:36:36 -0700

    excluding shakemaps

--- scripts/sqls/select_codelists.sql
+++ scripts/sqls/select_codelists.sql
@@ -35,6 +35,54 @@
 -- where c.status != 200;
 
 -- where are these code lists
+-- select distinct xpath
+-- from codelists;
 
-select distinct xpath
-from codelists;
+-- -- the which types of things are affected by inaccessible codelists?
+-- -- don't need to worry about redirects (aren't any)
+-- -- starting with a group by last xpath elem
+-- with c as 
+-- (
+-- 	select id, codelist
+-- 	from codelist_statuses
+-- 	where status > 200
+-- ), x as
+-- (
+-- 	select r.id as response_id, r.host, r.metadata_age,
+-- 		lower(replace(left(i.codelist, strpos(i.codelist, '#')), '#', '')) as codelist,
+-- 		i.xpath, string_to_array(i.xpath, '/') as xpath_arr
+-- 	from codelists i join responses r on r.source_url_sha = i.file
+-- )
+-- select count(x.response_id) as num_responses, 
+-- 	-- x.host, 
+-- 	-- x.metadata_age::date, 
+-- 	-- c.codelist,
+-- 	-- x.xpath, 
+-- 	x.xpath_arr[array_upper(xpath_arr, 1)] as last_x
+-- from x join c on c.codelist = x.codelist
+-- --order by x.codelist
+-- group by last_x
+-- order by num_responses DESC;
+
+-- -- and grouped by the full xpath (just in case)
+with c as 
+(
+	select id, codelist
+	from codelist_statuses
+	where status > 200
+), x as
+(
+	select r.id as response_id, r.host, r.metadata_age,
+		lower(replace(left(i.codelist, strpos(i.codelist, '#')), '#', '')) as codelist,
+		i.xpath, string_to_array(i.xpath, '/') as xpath_arr
+	from codelists i join responses r on r.source_url_sha = i.file
+)
+select count(distinct x.response_id) as num_responses, 
+	-- x.host, 
+	-- x.metadata_age::date, 
+	-- c.codelist,
+	x.xpath
+from x join c on c.codelist = x.codelist
+--order by x.codelist
+group by x.xpath
+order by num_responses DESC;
