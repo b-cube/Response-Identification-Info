fbeedb5bee496c7b979001a907c7dc9bc23617ba
roomthily <sorenscott@gmail.com>
2015-11-16 09:32:01 -0700

    schema stats

---
+++ scripts/sqls/completeness_tokens (Soren’s MacBook Pro's conflicted copy 2015-11-15).sql
@@ -0,0 +1,83 @@
+﻿-- "{"lineage": 
+--	[{"tokens": 18, 
+--	"text": "Remote Sensing Systems Inc, JAXA, NASA, OSDPD, CoastWatch 2013-12-01T19:15:23Z NOAA CoastWatch (West Coast Node) and NOAA SFSC ERD", 
+--	"tag": "MI_Metadata/dataQualityInfo/DQ_DataQuality/lineage/LI_Lineage/statement/CharacterString"}], 
+--  "attributes": [], "data_quality": []}"
+
+-- with lineage_tokens as
+-- (
+-- 	select response_id, lineage
+-- 	from metadata_completeness, json_array_elements(wordcounts->'lineage') as lineage
+-- 	where (wordcounts->'lineage')::text != '[]'
+-- ), quality_tokens as
+-- (
+-- 	select response_id, data_quality
+-- 	from metadata_completeness, json_array_elements(wordcounts->'data_quality') as data_quality
+-- 	where (wordcounts->'data_quality')::text != '[]'
+-- ), i as (
+-- 	select d.response_id, (e.value->'protocol')::text as ident
+--    	from identities d, jsonb_array_elements(d.identity::jsonb) e
+-- 	where d.identity is not null 
+-- 		and (e.value->>'protocol' = 'ISO' or e.value->>'protocol' = 'FGDC')
+-- )
+-- select r.id, date_trunc('month', r.metadata_age)::date as age,
+-- 	min(i.ident),
+-- 	sum((x.lineage->'tokens')::text::int) as num_lineage_tokens,
+-- 	count(x.lineage->'tokens') as num_lineage_elements,
+-- 	sum((y.data_quality->'tokens')::text::int) as num_quality_tokens,
+-- 	count(y.data_quality->'tokens') as num_quality_elements
+-- from responses r join i on i.response_id = r.id
+-- 	left outer join lineage_tokens x on x.response_id = r.id
+-- 	left outer join quality_tokens y on y.response_id = r.id
+-- where i.ident = '"ISO"' or i.ident = '"FGDC"'
+-- group by r.id;
+
+-- select response_id, data_quality
+-- from metadata_completeness, 
+-- 	json_array_elements(wordcounts->'data_quality') as data_quality
+-- limit 50;
+
+
+	-- with quality_tokens as
+-- 	(
+-- 		select response_id, data_quality
+-- 		from metadata_completeness, json_array_elements(wordcounts->'data_quality') as data_quality
+-- 		where (wordcounts->'data_quality')::text != '[]'
+-- 	), i as (
+-- 		select d.response_id, (e.value->'protocol')::text as ident
+-- 		from identities d, jsonb_array_elements(d.identity::jsonb) e
+-- 		where d.identity is not null 
+-- 			and e.value->>'protocol' = 'ISO'
+-- 	)
+-- 	select r.id, date_trunc('month', r.metadata_age)::date as age,
+-- 		min(i.ident) as protocol,
+-- 		sum((y.data_quality->'tokens')::text::int) as num_quality_tokens,
+-- 		count(y.data_quality->'tokens') as num_quality_elements
+-- 	from responses r join i on i.response_id = r.id
+-- 		left outer join quality_tokens y on y.response_id = r.id
+-- 	where i.ident = '"ISO"'
+-- 	group by r.id
+-- 	order by num_quality_elements DESC;
+
+with lineage_tokens as
+(
+	select response_id, lineage
+	from metadata_completeness, json_array_elements(wordcounts->'lineage') as lineage
+	where (wordcounts->'lineage')::text != '[]'
+), i as (
+	select d.response_id, (e.value->'protocol')::text as ident
+	from identities d, jsonb_array_elements(d.identity::jsonb) e
+	where d.identity is not null 
+		and e.value->>'protocol' = 'ISO'
+)
+select r.id, r.host, date_trunc('month', r.metadata_age)::date as age,
+	min(i.ident) as protocol,
+	sum((y.lineage->'tokens')::text::int) as num_lineage_tokens,
+	count(y.lineage->'tokens') as num_lineage_elements
+from responses r join i on i.response_id = r.id
+	left outer join lineage_tokens y on y.response_id = r.id
+where i.ident = '"ISO"'
+group by r.id, age
+order by num_lineage_elements DESC;
+
+--19689
---
+++ scripts/sqls/completeness_tokens_lineagestatement (Soren’s MacBook Pro's conflicted copy 2015-11-15).sql
@@ -0,0 +1,86 @@
+﻿-- -- count of how many iso this affects
+-- with i as (
+-- 	select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+-- 	from identities d
+-- 	where d.identity is not null
+-- ), m as 
+-- (
+-- 	select response_id, existences->'lineage' as has_lineage,
+-- 		j.* as tokens
+-- 	from metadata_completeness, json_array_elements(wordcounts->'lineage') j
+-- )
+-- 
+-- select count(distinct m.response_id) --, m.has_lineage, m.value->'tokens' as num_tokens_in_statement
+-- from m join i on i.response_id = m.response_id
+-- where i.ident->>'protocol' = 'ISO'
+-- 	and m.value::jsonb->>'tag'::text like '%/dataQualityInfo/DQ_DataQuality/lineage/LI_Lineage/statement/CharacterString'
+-- ;
+
+
+-- -- rolling it into (has_lineage or has token for tag) for the raw counts
+-- with i as (
+-- 	select d.response_id, (e.value->'protocol')::text as ident
+-- 	from identities d, jsonb_array_elements(d.identity::jsonb) e
+-- 	where d.identity is not null 
+-- 		and (e.value->>'protocol' = 'ISO' or e.value->>'protocol' = 'FGDC' )
+-- ), x as 
+-- (
+-- 	with m as
+-- 	(
+-- 		select response_id, j.value as lineage
+-- 		from metadata_completeness, json_array_elements(wordcounts->'lineage') j
+-- 		where wordcounts->>'lineage' != '[]'
+-- 	)
+-- 	select m.response_id, m.lineage->'tokens' as statement_tokens
+-- 	from m 
+-- 	where (m.lineage->>'tag')::text ilike '%/dataQualityInfo/DQ_DataQuality/lineage/LI_Lineage/statement/CharacterString%'
+-- )
+-- 
+-- select --c.response_id, 
+-- -- 	(c.existences->'lineage')::text::boolean as has_lineage,
+-- -- 	x.statement_tokens
+-- 	case when 
+-- 		(c.existences->'lineage')::text::boolean = True 
+-- 			or x.statement_tokens is not null 
+-- 		then True 
+-- 		else False 
+-- 	end as the_lineage,
+-- 	count(c.response_id),
+-- 	i.ident
+-- from metadata_completeness c 
+-- 	left outer join x on x.response_id = c.response_id
+-- 	left outer join i on i.response_id = c.response_id
+-- group by i.ident, the_lineage;
+
+with i as (
+	select d.response_id, (e.value->'protocol')::text as ident
+	from identities d, jsonb_array_elements(d.identity::jsonb) e
+	where d.identity is not null 
+		and e.value->>'protocol' = 'ISO'
+), x as 
+(
+	with m as
+	(
+		select response_id, j.value as lineage
+		from metadata_completeness, json_array_elements(wordcounts->'lineage') j
+		where wordcounts->>'lineage' != '[]'
+	)
+	select m.response_id, m.lineage->'tokens' as statement_tokens
+	from m 
+	where (m.lineage->>'tag')::text ilike '%/dataQualityInfo/DQ_DataQuality/lineage/LI_Lineage/statement/CharacterString%'
+)
+
+select r.host,
+	case 
+		when (c.existences->'lineage')::text::boolean = True then 'Processing Steps'
+		when x.statement_tokens is not null then 'Statement'
+		else 'No Lineage'
+	end as lineage_type,
+	count(c.response_id)
+from metadata_completeness c 
+	left outer join x on x.response_id = c.response_id
+	join i on i.response_id = c.response_id
+	join responses r on r.id = c.response_id
+where i.ident = '"ISO"'
+group by r.host, lineage_type
+order by r.host, lineage_type;
