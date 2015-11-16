cc7edab4d93e0eea554820bfc80b31783b76bb25
roomthily <sorenscott@gmail.com>
2015-11-16 12:35:41 -0700

    rest of the schema analysis

---
+++ scripts/sqls/select_schemas.sql
@@ -0,0 +1,12 @@
+﻿-- select s.status_code, s.schema, r.id, r.source_url, r.schemas
+-- from responses r
+-- 	join schema_status s on s.schema = ANY(r.schemas)
+-- where r.format = 'xml' and r.schemas is not null and r.schemas != '{}';
+
+-- how many responses contain a reference to a linkrot schema
+select s.status_code, count(distinct r.id) as num_responses --, r.source_url, r.schemas
+from responses r
+	join schema_status s on s.schema = ANY(r.schemas)
+where r.format = 'xml' and r.schemas is not null and r.schemas != '{}'
+	and s.status_code >= 400
+group by s.status_code;
