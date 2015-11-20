39da6cf7d7d45bdb8ce9f07690c3bccacad2e8d2
roomthily <sorenscott@gmail.com>
2015-11-19 21:17:31 -0700

    the basic questions have a start

--- scripts/sqls/uids_basic.sql
+++ scripts/sqls/uids_basic.sql
@@ -22,26 +22,43 @@
 -- group by protocol, r.host
 -- order by num desc;
 
-with i as (
-	select d.response_id, (e.value->'protocol')::text as ident
-	from identities d, jsonb_array_elements(d.identity::jsonb) e
-	where d.identity is not null
-), j as 
-(
-	select r.id, r.host, trim(both '"' from i.ident) as protocol
-	from responses r join i on i.response_id = r.id
-), k as (
-	select j.host, j.protocol, count(distinct j.id) as total
-	from j 
-	group by j.host, j.protocol
-)
+-- with i as (
+-- 	select d.response_id, (e.value->'protocol')::text as ident
+-- 	from identities d, jsonb_array_elements(d.identity::jsonb) e
+-- 	where d.identity is not null
+-- ), j as 
+-- (
+-- 	select r.id, r.host, trim(both '"' from i.ident) as protocol
+-- 	from responses r join i on i.response_id = r.id
+-- ), k as (
+-- 	select j.host, j.protocol, count(distinct j.id) as total
+-- 	from j 
+-- 	group by j.host, j.protocol
+-- )
+-- 
+-- select j.protocol, j.host, count(distinct j.id) as count_w_doi,
+-- 	round(count(distinct j.id) / max(k.total)::numeric * 100., 2) as pct_w_doi,
+-- 	max(k.total) as total_responses
+-- from j 
+-- 	inner join unique_identifiers u on u.response_id = j.id
+-- 	join k on k.host = j.host and k.protocol = j.protocol
+-- where u.match_type = 'doi'
+-- group by j.protocol, j.host
+-- order by j.host, j.protocol, pct_w_doi desc;
 
-select j.protocol, j.host, count(distinct j.id) as count_w_doi,
-	round(count(distinct j.id) / max(k.total)::numeric * 100., 2) as pct_w_doi,
-	max(k.total) as total_responses
-from j 
-	inner join unique_identifiers u on u.response_id = j.id
-	join k on k.host = j.host and k.protocol = j.protocol
-where u.match_type = 'doi'
-group by j.protocol, j.host
-order by j.host, j.protocol, pct_w_doi desc;
+with j as (
+	with i as 
+	(
+		select response_id, count(potential_identifier) as potentials
+		from unique_identifiers
+		group by response_id
+	)
+	select r.id, r.source_url, i.potentials, u.potential_identifier, u.match_type
+	from responses r 
+		join i on i.response_id = r.id
+		join unique_identifiers u on u.response_id = r.id
+	where i.potentials = 1 and u.match_type = 'url'
+)
+select count(j.id) as num
+from j
+where j.source_url = j.potential_identifier;
