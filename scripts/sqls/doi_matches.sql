095a7a4ced42a04e00d01ec254d16c0d35b4235a
roomthily <sorenscott@gmail.com>
2015-11-20 12:20:48 -0700

    simhashes are kinda fun

--- scripts/sqls/doi_matches.sql
+++ scripts/sqls/doi_matches.sql
@@ -1,12 +1,12 @@
-﻿with u as 
-(
-	select id, response_id, potential_identifier
-	from unique_identifiers
-	where match_type = 'doi'
-)
+﻿-- select a.potential_identifier, a.response_id, b.response_id
+-- from unique_identifiers a 
+-- 	left outer join unique_identifiers b on b.potential_identifier = a.potential_identifier
+-- where a.match_type = 'doi' and a.response_id != b.response_id;
 
-select a.potential_identifier, a.response_id, u.response_id
-from unique_identifiers a 
-	left outer join u 
-		on u.potential_identifier = a.potential_identifier and u.response_id != a.response_id
-;
+-- let's just cut out a bunch of things we're not interested in
+-- select * into doi_identifiers from unique_identifiers where match_type = 'doi';
+
+select a.potential_identifier, a.response_id, b.response_id
+from doi_identifiers a
+	left outer join doi_identifiers b on b.potential_identifier = a.potential_identifier
+where b.response_id != a.response_id;
