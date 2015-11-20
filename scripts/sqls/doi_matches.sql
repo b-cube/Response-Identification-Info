83d9e0db96f2e7716fb894a21c84c8d6499d5b53
roomthily <sorenscott@gmail.com>
2015-11-20 01:29:27 -0700

    even i am surprised at how few of the things seem to be out there

---
+++ scripts/sqls/doi_matches.sql
@@ -0,0 +1,4 @@
+﻿select a.potential_identifier, a.response_id, b.response_id
+from unique_identifiers a 
+	left outer join unique_identifiers b on b.potential_identifier = a.potential_identifier
+where a.match_type = 'doi' and a.response_id != b.response_id;
