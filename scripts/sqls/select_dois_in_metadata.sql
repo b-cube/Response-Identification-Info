70035f1d490228e2e2b2f4e75f7f3a73702cad40
roomthily <sorenscott@gmail.com>
2015-11-21 18:17:51 -0700

    ogc tokens

---
+++ scripts/sqls/select_dois_in_metadata.sql
@@ -0,0 +1,11 @@
+﻿select m.response_id as source_id, 
+	u.response_id as found_id,
+	m.potential_identifier, 
+	m.protocol as source_protocol,
+	r.source_url as source_url,
+	k.source_url as found_url
+from doi_identifiers_in_md m
+	inner join doi_identifiers u on m.potential_identifier = u.potential_identifier
+	join responses r on m.response_id = r.id
+	join responses k on u.response_id = k.id
+where m.potential_identifier != 'http://dx.doi.org/';
--- scripts/sqls/select_unique_identifiers.sql
+++ scripts/sqls/select_unique_identifiers.sql
@@ -12,19 +12,69 @@
 -- order by avg_ids DESC;
 
 -- how many have one identifier and it's a url and it's the source url?
-with j as (
-	with i as 
-	(
-		select response_id, count(potential_identifier) as potentials
-		from unique_identifiers
-		group by response_id
+-- with j as (
+-- 	with i as 
+-- 	(
+-- 		select response_id, count(potential_identifier) as potentials
+-- 		from unique_identifiers
+-- 		group by response_id
+-- 	)
+-- 	select r.id, r.source_url, i.potentials, u.potential_identifier, u.match_type
+-- 	from responses r 
+-- 		join i on i.response_id = r.id
+-- 		join unique_identifiers u on u.response_id = r.id
+-- 	where i.potentials = 1 and u.match_type = 'url'
+-- )
+-- select j.id, j.source_url
+-- from j
+-- where j.source_url = j.potential_identifier;
+
+-- with i as 
+-- (
+-- 	select response_id, tag, match_type, count(potential_identifier) as num
+-- 	from unique_identifiers
+-- 	where match_type != 'url' and tag != ''
+-- 		and (
+-- 			tag ilike 'MI_Metadata/%'
+-- 			or tag ilike 'MD_Metadata/%'
+-- 			or tag ilike 'metadata/%'
+-- 			or tag ilike 'OAI-PMH/%'
+-- 			or tag ilike 'SensorML/%'
+-- 			or tag ilike '%_Capabilities/%'
+-- 			or tag ilike 'Capabilities/%'
+-- 			or tag ilike 'DescribeSensorResponse/%'
+-- 			or tag ilike 'DIF/%'
+-- 			or tag ilike 'DS_Series/%'
+-- 			or tag ilike 'eml/%'
+-- 			or tag ilike 'GetRecordByIdResponse/%'
+-- 			or tag ilike 'GetRecordsResponse/%'
+-- 		)
+-- 	group by response_id, tag, match_type
+-- )
+-- 
+-- select tag, match_type, round(avg(num), 2) as avg_ids
+-- from i
+-- group by tag, match_type
+-- order by tag, avg_ids DESC;
+
+
+select tag, match_type, count(potential_identifier) as num
+from unique_identifiers
+where match_type != 'url' and tag != ''
+	and (
+		tag ilike 'MI_Metadata/%'
+		or tag ilike 'MD_Metadata/%'
+		or tag ilike 'metadata/%'
+		or tag ilike 'OAI-PMH/%'
+		or tag ilike 'SensorML/%'
+		or tag ilike '%_Capabilities/%'
+		or tag ilike 'Capabilities/%'
+		or tag ilike 'DescribeSensorResponse/%'
+		or tag ilike 'DIF/%'
+		or tag ilike 'DS_Series/%'
+		or tag ilike 'eml/%'
+		or tag ilike 'GetRecordByIdResponse/%'
+		or tag ilike 'GetRecordsResponse/%'
 	)
-	select r.id, r.source_url, i.potentials, u.potential_identifier, u.match_type
-	from responses r 
-		join i on i.response_id = r.id
-		join unique_identifiers u on u.response_id = r.id
-	where i.potentials = 1 and u.match_type = 'url'
-)
-select j.id, j.source_url
-from j
-where j.source_url = j.potential_identifier;
+group by tag, match_type
+order by tag, num DESC;
