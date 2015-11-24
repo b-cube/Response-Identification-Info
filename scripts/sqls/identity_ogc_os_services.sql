6d4188e40bfe49ef10f7140996a78cb71142eed5
roomthily <sorenscott@gmail.com>
2015-11-24 00:19:02 -0700

    split out csw/os resultsets

---
+++ scripts/sqls/identity_ogc_os_services.sql
@@ -0,0 +1,19 @@
+﻿with j as (
+	select i.response_id, k.*
+	from identities i, jsonb_array_elements(i.identity::jsonb) k
+)
+select j.response_id, r.source_url, j.value,
+	trim(both '"' from (j.value->'protocol')::text) as protocol,
+	case 
+		when trim(both '"' from (j.value->'protocol')::text) = 'OpenSearch' and j.value ? 'service'
+			then trim(both '"' from (j.value->'service'->'name')::text)
+		when trim(both '"' from (j.value->'protocol')::text) = 'OpenSearch' and j.value ? 'resultset'
+			then 'Resultset'
+		when trim(both '"' from (j.value->'protocol')::text) = 'OGC' and j.value ? 'service'
+			then trim(both '"' from (j.value->'service'->'name')::text) || ':' || trim(both '"' from (j.value->'service'->'request')::text)
+		when trim(both '"' from (j.value->'protocol')::text) = 'OGC' and j.value ? 'dataset'
+			then trim(both '"' from (j.value->'dataset'->'name')::text) || ':' || trim(both '"' from (j.value->'dataset'->'request')::text)
+	end as description
+from j
+	join responses r on j.response_id = r.id
+where (j.value->>'protocol')::text = 'OGC' or (j.value->>'protocol')::text = 'OpenSearch';
---
+++ scripts/sqls/whats_up_csw.sql
@@ -0,0 +1,20 @@
+﻿with j as (
+	select response_id, match_type, count(potential_identifier)
+	from unique_identifiers
+	where response_id = 313708
+	or response_id = 398877
+	or response_id = 402232
+	or response_id = 436829
+	or response_id = 481130
+	or response_id = 528861
+	or response_id = 552613
+	or response_id = 575004
+	or response_id = 580997
+	or response_id = 658072
+	or response_id = 692544
+	or response_id = 692862
+	or response_id = 786259
+	group by response_id, match_type
+)
+select r.id, r.host, j.match_type, j.count
+from responses r join j on j.response_id = r.id;
