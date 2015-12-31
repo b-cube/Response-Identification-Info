3f3e9788fe7235021dc7fc5db65e8f4fbb32c5e8
roomthily <sorenscott@gmail.com>
2015-12-30 23:13:35 -0700

    distribution function codes by host

---
+++ notebooks/sql/iso_specific_details/select-distribution_function_codes_by_host.sql
@@ -0,0 +1,15 @@
+-- distribution function codes by host
+with j as 
+(
+    select response_id, element_value, count(id) as num_values
+    from iso_onlineresources
+    where element_key = 'function_code'
+        and tags ilike '%/distributionInfo%'
+    group by element_key, response_id, element_value
+)
+select element_value,
+    r.host,
+    count(distinct j.response_id) as num_responses
+from j join responses r on j.response_id = r.id
+group by r.host, element_value
+order by element_value, r.host, num_responses DESC;
