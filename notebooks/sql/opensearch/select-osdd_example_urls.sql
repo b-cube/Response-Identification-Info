98076bd51da986a58655b252843773ab437c4b6a
roomthily <sorenscott@gmail.com>
2015-12-31 08:48:54 -0700

    osdd example urls

---
+++ notebooks/sql/opensearch/select-osdd_example_urls.sql
@@ -0,0 +1,29 @@
+-- osdd with example urls
+with t 
+as (
+    select o.id, x.*
+    from osdds o, 
+        jsonb_to_recordset(o.url_templates::jsonb) 
+            as x(
+                default_url text,
+                search_url text,
+                example_url text,
+                responses jsonb,
+                accept_type text
+            )
+    where url_templates is not null and url_templates::text != '[]'::text
+)
+
+select round((example_response->'status')::text::int, -2) as code,
+    t.accept_type,
+    count(distinct t.example_url) as num
+from t,
+    jsonb_to_recordset(responses) as y(
+        search_response jsonb,
+        example_response jsonb,
+        default_response jsonb
+    )
+where example_response->>'status' != '{}'
+group by code, t.accept_type
+order by code asc, t.accept_type
+;
