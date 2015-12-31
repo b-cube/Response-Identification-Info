bd66d76998b98653e44146167b50a5ac1e8dec49
roomthily <sorenscott@gmail.com>
2015-12-31 12:15:37 -0700

    identified docs by host

---
+++ notebooks/sql/metadata_metrics/select-host_by_identified.sql
@@ -0,0 +1,20 @@
+-- who's serving what kind of identified things
+with i
+as (
+    select d.response_id, jsonb_array_elements(d.identity::jsonb) ident
+    from identities d
+    where d.identity is not null
+), h as (
+    select r.host, count(r.host) as total_for_host
+    from responses r
+    where r.host is not null and r.host != ''
+    group by r.host
+)
+select r.host,
+    count(i.ident->'protocol') as num_identified_by_protocol,
+    i.ident->'protocol' as protocol,
+    min(h.total_for_host) as total_for_host
+from responses r join i on i.response_id = r.id
+    join h on h.host = r.host
+group by r.host, protocol
+order by r.host asc, protocol desc;
